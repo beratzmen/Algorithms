47fb5c9c69ed250dc3e3c9d7a09abcb23e1c10f3
Berat Özmen <bzmen@hotmail.com>
2021-04-17 22:59:45 +0300

    girilen sayıdan 50 ye kadar olan sayıların faktoriyellerini hesaplayan console uygulaması

--- NFactorial/Program.cs
+++ NFactorial/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace NFactorial
 {
@@ -6,7 +7,34 @@ namespace NFactorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BigInteger factorial = 1;
+            uint number;
+            Console.Write("bir sayı giriniz: ");
+            number = Convert.ToUInt32(Console.ReadLine());
+
+            if (number <= 0 || number > 50)
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine("0' ın faktoriyeli = {0}", 1);
+                }
+                Console.WriteLine("girilen sayı 0 ile 50 arasında olmalı");
+            }
+            else
+            {
+                while (number <= 50)
+                {
+                    factorial = 1;
+                    for (uint i = number; i > 0; i--)
+                    {
+                        factorial *= i;
+                    }
+                    Console.WriteLine("Sayı {0} - faktoriyel {1}", number, factorial);
+                    number++;
+                }
+
+                Console.WriteLine(factorial);
+            }
         }
     }
 }
