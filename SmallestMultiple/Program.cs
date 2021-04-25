7f274c14d9588f19a2d807b7acb87897437563df
Berat Özmen <bzmen@hotmail.com>
2021-04-25 21:38:10 +0300

    lcm method

--- SmallestMultiple/Program.cs
+++ SmallestMultiple/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SmallestMultiple
 {
@@ -6,8 +7,11 @@ namespace SmallestMultiple
     {
         static void Main(string[] args)
         {
+            LcmV1();
+        }
 
-
+        private static void LcmV1()
+        {
             for (int i = 20; i < 999999999; i++)
             {
                 if ((i % 2 == 0) && (i % 3 == 0) && (i % 4 == 0) && (i % 5 == 0) && (i % 6 == 0) && (i % 7 == 0) && (i % 8 == 0) && (i % 9 == 0) && (i % 10 == 0) && (i % 11 == 0) && (i % 12 == 0) && (i % 13 == 0) && (i % 14 == 0) && (i % 15 == 0) && (i % 16 == 0) && (i % 17 == 0) && (i % 18 == 0) && (i % 19 == 0) && (i % 20 == 0))
@@ -15,7 +19,6 @@ namespace SmallestMultiple
                     Console.WriteLine(string.Format($"{i}"));
                 }
             }
-
         }
     }
 }
