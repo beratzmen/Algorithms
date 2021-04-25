400eccfcdbdd6012d94dc100db85bf2c8f243778
Berat Özmen <bzmen@hotmail.com>
2021-04-25 22:12:25 +0300

    1 ile 100 arasındaki sayıların, toplamının karesi ile kareleri toplamının farkını hesaplayan uygulama

---
+++ SumSquareDifference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumSquareDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int square = 1;
+
+            for (int i = 1; i < 101; i++)
+            {
+                sum += i;
+                square += i * i;
+            }
+
+            Console.WriteLine(string.Format($"Toplamlarının Karesi = {sum * sum} || Karelerinin Toplamı = {square - 1}"));
+            Console.WriteLine(string.Format($"Farkları = {(sum * sum) - (square - 1)}"));
+            Console.ReadLine();
+        }
+    }
+}
