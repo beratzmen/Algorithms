c77117f19705ec3ff29c83ce982d0f0dbf10c07c
Berat Özmen <bzmen@hotmail.com>
2021-04-30 21:48:35 +0300

    üçgensel sayıları bulan  ve bu sayıların çarpanlarını listeleyen uygulama

---
+++ HighlyDivisibleTriangularNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HighlyDivisibleTriangularNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sum = 0, number = 20;
+            List<long> triangularNumber = new List<long>();
+
+            for (int i = 1; i < number; i++)
+            {
+                sum += i;
+                triangularNumber.Add(sum);
+                Console.Write(i == number - 1 ? sum + "  " : sum + ",");
+            }
+
+            Console.WriteLine("\n");
+
+            for (long i = 0; i < triangularNumber.Count; i++)
+            {
+                Console.Write(string.Format($"{triangularNumber[Convert.ToInt32(i)],-3} =>"));
+                for (long j = 1; j <= triangularNumber[Convert.ToInt32(i)]; j++)
+                {
+                    if (triangularNumber[Convert.ToInt32(i)] % j == 0)
+                    {
+                        Console.Write(string.Format($" {j } "));
+
+                    }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
